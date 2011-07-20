cfa2f8d2e0dd5bae985a79a0fc1221b001786b49
(no author) <(no author)@025e8bb6-0a04-4741-bce8-018a994c586a>
2011-07-20 08:02:45 +0000

    aaaa

--- controllers/help_controller.php
+++ controllers/help_controller.php
@@ -5,21 +5,20 @@ class HelpController extends AppController{
   var $uses = array('Member');
 
   function page_1(){
-
+	echo 'a';
   }
 
   function page_2(){
-
+	echo 'b';
   }
 
   function page_3(){
-
+	echo 'c';
 
   }
 
   function page_4(){
-
-
+	echo 'd';
   }
 
 }
